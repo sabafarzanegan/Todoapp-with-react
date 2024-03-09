2b928a659dc4e693950fcc899e4139496f3fd8f4
saba farzanegan <saba.far.99@gmail.com>
2024-03-09 12:31:01 +0330

    create css and ui

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useState } from "react";
+import "./App.css"
+import Todos from "./components/todos/Todos";
 
 function App() {
+  const [inputvalue, setInputvalue] = useState("");
+  const inputhandler = (e) => {
+    setInputvalue(e.target.value);
+  };
+  const addhandler = (e) => {
+    e.preventDefault()
+  }
   return (
-    <div>App</div>
-  )
+    <>
+      <h1 className="apptitle">لیست کارها</h1>
+      <div className="app-container">
+        <form action="" onSubmit={addhandler}>
+          <input onChange={inputhandler} type="text" value={inputvalue} />
+          <button className="addbtn" type="submit">اضافه کردن</button>
+        </form>
+        <Todos />
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
--- src/components/todos/Todos.js
+++ src/components/todos/Todos.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import "./Todos.css"
+import { IoTrashBinOutline } from "react-icons/io5";
+import { FaCheck } from "react-icons/fa6";
 
 function Todos() {
   return (
-    <div>Todos</div>
-  )
+    <>
+      <div className="todos-container">
+        <ul>
+          <li className=''><span><FaCheck className='todoicon' /> <IoTrashBinOutline className='todoicon' /></span>ورزش کردن</li>
+          <li>ورزش کردن</li>
+          <li>ورزش کردن</li>
+        </ul>
+      </div>
+    </>
+  );
 }
 
 export default Todos
